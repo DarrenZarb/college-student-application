c1df1ee52a00f10b050fd416882b8feb44b4992b
DarrenZarb <darrenzarb98@hotmail.com>
2025-04-05 22:58:55 +0000

    Add migration for creating colleges table

--- database/migrations/2025_03_02_101057_create_colleges.php
+++ database/migrations/2025_03_02_101057_create_colleges.php
@@ -8,19 +8,21 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * This will create the 'colleges' table with appropriate fields
      */
     public function up(): void
     {
         Schema::create('colleges', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->string('address')->nullable(false);
-            $table->timestamps();
+            $table->id(); //Primary key with auto-increment
+            $table->string('name')->unique(); //required and must be unique
+            $table->string('address')->nullable(false); //College address, required
+            $table->timestamps(); // created_at and updated_at timestamps
         });
     }
 
     /**
      * Reverse the migrations.
+     * This will drop the 'colleges' table if it exists
      */
     public function down(): void
     {
